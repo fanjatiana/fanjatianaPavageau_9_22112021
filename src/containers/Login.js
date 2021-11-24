b089a66b0ab14f3ebe5dedd24cc9331caf727f4b
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-11-24 11:27:33 +0100

    correction du problème de connexion des administrateurs

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -31,14 +31,15 @@ export default class Login {
     PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
     this.document.body.style.backgroundColor="#fff"
   }
-
+  // correction du problème de connexion pour les administrateurs : e.target.queryselector("....employee" => "...admin")
   handleSubmitAdmin = e => {
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
+    
     this.localStorage.setItem("user", JSON.stringify(user))
     const userExists = this.checkIfUserExists(user)
     if (!userExists) this.createUser(user)
