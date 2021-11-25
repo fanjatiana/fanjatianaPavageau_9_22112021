cbc88cea437bfdda0934a1ee3dbae669c62cb957
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-11-25 15:18:26 +0100

    modification de la valeur de dateStr

--- src/app/format.js
+++ src/app/format.js
@@ -13,7 +13,7 @@ export const formatDate = (dateStr) => {
   date = date.getFullYear()+ "/" + (date.getMonth() + 1) + "/" + date.getDate();
   const dateIsValid = /^(19|20)\d\d[- ./](0?[1-9]|1[012])[- ./](0?[1-9]|[12][0-9]|3[01])$/
   if (!dateIsValid.test(date)){
-    return dateStr = "2000/01/01"
+    return dateStr = 0
   }
   return date
 }
