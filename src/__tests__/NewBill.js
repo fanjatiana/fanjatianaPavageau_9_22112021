c289d6096bcf7fb355ce0163d40daafea9387fc1
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-12-03 23:04:22 +0100

    premier test newBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,13 +2,19 @@ import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
+/*const onNavigate = (pathname) => {
+  document.body.innerHTML = ROUTES({ pathname });
+};
+//On indique que l'utilisateur est un Employee
+Object.defineProperty(window, "localStorage", { value: localStorageMock });
+window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));*/
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Then, Loading page should be rendered", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      expect(screen.getAllByText("Envoyer une note de frais")).toBeTruthy();
     })
   })
 })
